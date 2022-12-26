2fa9af231f5582b440c558a706470588f998d0ab
Louis-Vu <hoangvuhuy04@gmail.com>
2022-12-26 11:14:38 +0700

    exam

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ExamComponent} from "./exam/exam.component";
+import {FavoritesComponent} from "./exam/favorite/favorite.component";
+import {JohnComponent} from "./exam/john/john.component";
+import {RouterModule, Routes} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
+const appRoutes: Routes = [
+  {path: 'favorites',component:ExamComponent},
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,ExamComponent,FavoritesComponent,JohnComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
